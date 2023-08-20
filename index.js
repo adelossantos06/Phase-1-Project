05803e901ead63bea3ac98b666c0940f100557da
Alyssa De Los Santos <adelossantos6@gmail.com>
2023-08-20 08:28:44 -0500

    adds successful fetch execution

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => console.log("We are connected!"))
+
+function fetchData() {
+    fetch("https://restcountries.com/v3.1/all?fields=name,flags")
+        .then(res => res.json())
+        .then(data => {
+            console.log("data", data)
+        })
+}
+
+fetchData()
