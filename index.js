85f28f73865be81ac22d434be82a5c1c237654c9
Alyssa De Los Santos <adelossantos6@gmail.com>
2023-08-20 11:24:20 -0500

    adds CSS

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => console.log("We are connected!"))
 
+const countries = document.querySelector("#countires")
+
 function fetchData() {
     fetch("https://restcountries.com/v3.1/all?fields=name,flags")
         .then(res => res.json())
         .then(data => {
             console.log("data", data)
+            console.log((data[0]))
+            const countryLi = document.createElement('li')
+
         })
 }
 
 fetchData()
+
