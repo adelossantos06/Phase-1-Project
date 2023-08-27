4fe9eb55355df7c047c9172ec1ca3a77702793a5
Alyssa De Los Santos <adelossantos6@gmail.com>
2023-08-26 19:54:10 -0500

    adds working search

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 backround image url : https://wallpaperaccess.com/full/2391514.jpg
 
-API url : https://restcountries.com/v3.1/all?fields=name,flags
+API url : https://restcountries.com/v3.1/all?fields=name,flags,population,capital,continents
 
--- index.js
+++ index.js
@@ -1,60 +1,110 @@
-//document.addEventListener("DOMContentLoaded", () => console.log("We are connected!"));
-
-//const countries = document.querySelector("#countires")
-const form = document.querySelector("#submitBtn")
-const infoContainer = document.querySelector("#cards-contianer")
 const container = document.querySelector(".container")
-const search = document.querySelector("#search-submit")
-const searchInput = document.querySelector('#search')
-const submitBtn = document.querySelector("#submitBtn")
-const card = document.querySelector(".card")
-const cardTemplate = document.querySelector("#card-template")
+const outerCountryDiv = document.querySelector(".outer")
+const form = document.querySelector('#search-submit')
 const searchBar = document.querySelector('#searchBar')
 
-// let countries = []
-
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    console.log('form is submitted')
-})
-
-
+//let countries
 
 function fetchData() {
-    fetch("https://restcountries.com/v3.1/all")
-        .then(res => res.json())
-        .then(data => {
-            data.map(country => {
-                const div = container.appendChild(document.createElement('div'))
-                const divForNameImg = div.appendChild(document.createElement("div"))
-                const divForLi = div.appendChild(document.createElement('div'))
-                const image = document.createElement('img')
-                const name = document.createElement('h2')
-                const population = document.createElement('li')
-                const continents = document.createElement('li')
-                const capital = document.createElement('li')
+    form.addEventListener("submit", (e) => {
+        e.preventDefault()
+        console.log('submitted')
 
-                div.classList = 'card'
-                image.classList = 'card-img'
-                divForLi.classList = 'info-list'
+        //const renderdFilterdCountries = renderCountries(filteredData)
+        // fetchData(renderdFilterdCountries)
 
-                image.src = country.flags.png
-                image.alt = country.flags.alt
-                name.innerText = `${country.name.common}`
-                population.innerText = `Population: ${country.population}`
-                continents.innerText = `Continent: ${country.continents}`
-                capital.innerText = `Capital: ${country.capital}`
+        fetch("https://restcountries.com/v3.1/all?fields=name,flags,population,capital,continents")
+            .then(res => res.json())
+            .then(data => {
+                // console.log('data', data)
+                countries = data
+                // renderCountries(data)
+                const filteredData = countries.filter(country => country.name.common.includes(searchBar.value))
+                console.log(filteredData)
 
-                divForNameImg.append(name, image)
-                divForLi.append(population, continents, capital)
+                //outerCountryDiv.innerHTML = '';
 
+                renderCountries(filteredData)
             })
-
-
-        })
-
-        .catch(err => console.log('Request Failed', err));
+    })
 }
 
+//         .catch(err => ('Request Failed', err));
+// }
+
+
+function renderCountries(countries) {
+
+
+    countries.forEach(country => {
+        const div = container.appendChild(document.createElement('div'))
+        const image = document.createElement('img')
+        const divForNameImg = div.appendChild(document.createElement('div'))
+        const divForLi = div.appendChild(document.createElement('div'))
+        const name = document.createElement('h2')
+        const population = document.createElement('li')
+        const continents = document.createElement('li')
+        const capital = document.createElement('li')
+
+
+
+
+        div.classList = 'card'
+        image.classList = 'card-img'
+        divForLi.classList = 'info-list'
+
+        image.src = country.flags.png
+        image.alt = country.flags.alt
+        name.innerText = `${country.name.common}`
+        population.innerText = `Population: ${country.population}`
+        continents.innerText = `Continent: ${country.continents}`
+        capital.innerText = `Capital: ${country.capital[0]}`
+
+
+        divForNameImg.append(name, image)
+        divForLi.append(population, continents, capital)
+
+    })
+}
+
+// function handleFilterSubmit(e) {
+//     e.preventDefault()
+// const filterCriteria = e.target.value
+// // fetchData()
+// //     .then(data => {
+// //         console.log('data', data)
+// const filteredData = data.filter(country => country.name.common.includes(filterCriteria))
+
+//         console.log('filterd', filteredData)
+//renderCountries(filteredData)
+//     })
+
+// }
+
+
+
+//form.addEventListener("submit", handleFilterSubmit)
+
+
+
+
+//form.addEventListener("submit", handleFilterSubmit)
+
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault()
+//     console.log('submitted')
+//     const filteredData = countries.filter(country => country.name.common.includes(searchBar.value))
+//     console.log(filteredData)
+//     const renderdFilterdCountries = renderCountries(filteredData)
+//     fetchData(renderdFilterdCountries)
+// })
+
+// function handleFilterSubmit(e) {
+//     e.preventDefault()
+//     const filteredData = countries.filter(country => country.name.common.includes(searchBar.value))
+//     console.log(filteredData)
+//     fetchData(filteredData)
+// }
 fetchData()
+
+//handleFilterSubmit()
